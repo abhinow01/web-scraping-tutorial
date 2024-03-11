5b2fda0d1956471a325a5feb9263bcee40966dac
abhinav khanna <kabhinav99@gmail.com>
2024-03-12 02:32:25 +0530

    converts to csv

--- books.js
+++ books.js
@@ -1,29 +1,35 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const j2cp = require("json2csv").Parser;
+const fs = require("fs");
 
 const mystery = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html";
 const baseUrl = "https://books.toscrape.com/catalogue/category/books/mystery_3/";
 
 const book_data = [];
 
-async function getBooks(url){
-    try{
+async function getBooks(url) {
+    try {
         const response = await axios.get(url);
         const $ = cheerio.load(response.data);
         const books = $("article");
-        books.each(function(){
+        books.each(function () {
             title = $(this).find("h3 a").text();
             price = $(this).find(".price_color").text();
             stock = $(this).find(".availability").text();
-            book_data.push({title,price,stock});
+            book_data.push({ title, price, stock });
         })
-        // console.log(book_data);
-        if($(".next a").length > 0){
-            next_page = baseUrl + $("next a").attr("href");
+        
+        if ($(".next a").length > 0) {
+            next_page = baseUrl + $(".next a").attr("href"); // Corrected selector
             getBooks(next_page);
+        } else {
+            const parser = new j2cp();
+            const csv = parser.parse(book_data);
+            fs.writeFileSync("./books.csv", csv);
         }
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
